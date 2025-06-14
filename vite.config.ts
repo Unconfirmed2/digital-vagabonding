5ab9ed4d8d8f0d88a454be70517ff7549289a0b8
Unconfirmed2 <eugene.turetsky@gmail.com>
2025-06-14 18:26:20 -0400

    Change Base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/dUnconfirmed2/',
+  base: '/Unconfirmed2/digital-vagabonding/',
   server: {
     host: "::",
     port: 8080,
