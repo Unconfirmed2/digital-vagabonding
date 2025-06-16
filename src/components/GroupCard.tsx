feec685d3ed24f5e5128b7f211e2370b67214356
Unconfirmed2 <eugene.turetsky@gmail.com>
2025-06-16 02:25:38 +0000

    fix: use relative paths for platform images in GroupCard

--- src/components/GroupCard.tsx
+++ src/components/GroupCard.tsx
@@ -22,12 +22,12 @@ interface GroupCardProps {
 }
 
 const platformImageMap: Record<string, string> = {
-  whatsapp: '/digital-vagabonding/Whatsapp.png',
-  telegram: '/digital-vagabonding/Telegram.png',
-  facebook: '/digital-vagabonding/Facebook.png',
-  meetup: '/digital-vagabonding/Meetup.png',
-  reddit: '/digital-vagabonding/Reddit.png',
-  internet: '/digital-vagabonding/Website.png',
+  whatsapp: 'Whatsapp.png',
+  telegram: 'Telegram.png',
+  facebook: 'Facebook.png',
+  meetup: 'Meetup.png',
+  reddit: 'Reddit.png',
+  internet: 'Website.png',
 };
 
 export const GroupCard: React.FC<GroupCardProps> = ({ group }) => {
